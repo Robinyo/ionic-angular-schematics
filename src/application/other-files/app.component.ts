e6eec77a2014a9fd5dbb1c85f9ea9e2355b5a816
Rob Ferguson <rob.ferguson@robferguson.org>
2018-01-03 17:06:49 +1100

    Updated the Application collection

--- src/application/other-files/app.component.ts
+++ src/application/other-files/app.component.ts
@@ -1,35 +1,25 @@
 import { Component } from '@angular/core';
 
+import { Platform } from 'ionic-angular';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from './pages/home/home.page';
+
 @Component({
-  selector: '<%= selector %>',<% if(inlineTemplate) { %>
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://github.com/angular/angular-cli/wiki">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <% if (routing) {
-     %><router-outlet></router-outlet><%
-    } %>
-  `,<% } else { %>
-  templateUrl: './app.component.html',<% } if(inlineStyle) { %>
-  styles: []<% } else { %>
-  styleUrls: ['./app.component.<%= styleext %>']<% } %>
+  templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  title = '<%= prefix %>';
+export class MyApp {
+
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
 }
---
+++ src/application/other-files/pages/home/home.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+// @IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: './home.page.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+  // public ionViewDidLoad() {}
+}
